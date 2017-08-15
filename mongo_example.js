6b6f32dec4521ee20c3edefc7c3fbdef28b60e4d
Nam Yoo <namgooy@gmail.com>
2017-08-15 01:01:51 +0000

    Acess Data(MongoDB) from Node.js

--- mongo_example.js
+++ mongo_example.js
@@ -13,11 +13,23 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
   //     starting here.
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
-  // ==> Let's "get all the tweets". In Mongo-speak, we "find" them.
-  db.collection("tweets").find().toArray((err, results) => {
-   if (err) throw err;
+ // ==> Refactored and wrapped as new, tweet-specific function:
 
-   console.log("results array: ", results);
+  function getTweets(callback) {
+    db.collection("tweets").find().toArray(callback);
+  }
+
+  // ==> Later it can be invoked. Remember even if you pass
+  //     `getTweets` to another scope, it still has closure over
+  //     `db`, so it will still work. Yay!
+
+  getTweets((err, tweets) => {
+    if (err) throw err;
+
+    console.log("Logging each tweet:");
+    for (let tweet of tweets) {
+      console.log(tweet);
+    }
 
     // ==> At the end, we close the connection:
     db.close();
